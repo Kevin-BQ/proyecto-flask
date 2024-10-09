a836942166a5c7b7c82270795173b8d7e8816a99
Kevin-BQ <kevinalexanderbricenoquezada@gmail.com>
2024-10-09 07:28:26 -0500

    add necessary changes

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from utils.db import db
 from config import URL_DATABASE
 
-
 app = Flask(__name__)
 
 app.secret_key = 'secret key'
@@ -12,7 +11,7 @@ app.secret_key = 'secret key'
 app.config["SQLALCHEMY_DATABASE_URI"] = (
     URL_DATABASE
 )
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 
 db.init_app(app)
 
